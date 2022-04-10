1531f40d46d2347f1d3fd3f59b0e7adedfb9d995
Justin Li <justinlit23@gmail.com>
2022-04-09 20:03:08 -0700

    commit tree 9ac243423a1e692fd3d7bdb542d76d09de880233

---
+++ index.js
@@ -0,0 +1,8 @@
+const header1 = document.createElement('h1')
+document.body.append('header1')
+
+const par1 = document.createElement('p')
+document.body.append(par1)
+
+header1.textContent = "My HTML adventure"
+par1.textContent = "We're writing HTML markup to display in our <strong>browser</strong>. We're basically telling computers what to do. <em>Neat!</em>"
